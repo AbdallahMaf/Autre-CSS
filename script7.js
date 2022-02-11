01a5f967f98a05a04de40cedd5226c4e751e52f8
Mohamed Abdallah Fall <abdallahfall2020@gmail.com>
2022-02-11 22:58:16 +0000

    second commit

--- script7.js
+++ script7.js
@@ -5,18 +5,21 @@ var green = document.querySelector('div#loader .green');
 
 
 function randomColor() {
-  return '#'+Math.random().toString(16).substr(-6);
+	return '#'+Math.random().toString(16).substr(-6);
 }
-  
+	
 // Toggle button code. Don't edit.
 var loader = document.getElementById("loader");
 var button = document.getElementById("button");
 button.addEventListener("click", function() {
-  if (loader.className === "loading") {
-    loader.className = "";
-  } else {
-    loader.className = "loading";
-  }
+	if (loader.className === "loading") {
+		loader.className = "";
+	} else {
+		red.style.background = randomColor()
+		blue.style.background = randomColor()
+		green.style.background = randomColor()
+		loader.className = "loading";
+	}
 })
 
 //loader.className ="";
